3b3660d6cc8143b332148731cdca3f5b283c6a36
Ulises Lara <lara_ulises_@outlook.com>
2022-05-01 19:21:02 -0500

    Implement method to get all Commands, in mock Repo

--- src/CommandAPI/Data/MockCommandAPIRepo.cs
+++ src/CommandAPI/Data/MockCommandAPIRepo.cs
@@ -17,7 +17,29 @@ namespace CommandAPI.Data
 
         public IEnumerable<Command> GetAllCommands()
         {
-            throw new System.NotImplementedException();
+            var commands = new List<Command>
+            {
+                new Command
+                {
+                    Id = 0, HowTo = "How to generate a migration",
+                    CommandLine = "dotnet ef migrations add <Name of Migration>",
+                    Platform = ".Net Core EF"
+                },
+                new Command
+                {
+                    Id = 1, HowTo = "Run Migrations",
+                    CommandLine = "dotnet ef database update",
+                    Platform = ".Net Core EF"
+                },
+                new Command
+                {
+                    Id = 2, HowTo = "List active migrations",
+                    CommandLine = "dotnet ef migrations list",
+                    Platform = ".Net Core EF"
+                }
+            };
+
+            return commands;
         }
 
         public Command GetCommandById(int id)
