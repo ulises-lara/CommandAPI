9de3d89d1cffa6e8f8687bfab2491dceb7657168
Ulises Lara <lara_ulises_@outlook.com>
2022-05-02 17:23:40 -0500

    Implement method to get all Commands, via mock Repo

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
@@ -13,6 +14,14 @@ namespace CommandAPI.Controllers
         public CommandsController(ICommandAPIRepo repository)
         {
             _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(commandItems);
         }
     }
 }
